b68e5fd59e7e66dcc733927253ce18643fad29f6
Shua Miller <thejoshmill@gmail.com>
2020-09-22 11:53:08 -0700

    Create 'gridReset' function with code and add event listener to 'button'.

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const gridSquare = document.createElement('div');
-const container = document.querySelector('.container');
-gridSquare.className = "grid-square";
-
 for (let i = 0; i < 256; i++) {
+    const gridSquare = document.createElement('div');
+    const container = document.querySelector('.container');
+    gridSquare.className = "grid-square";
     container.appendChild(gridSquare);
+    gridSquare.addEventListener('mouseover', function(e) {
+        e.target.className = 'hovered';
+    })
 }
+
+function gridReset() {
+    let gridSize = prompt("How many boxes per side do you want?");
+    const container = document.querySelector('.container');
+    container.setAttribute('style', `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr)`);
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    for(let i = 0; i < gridSize * gridSize; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.className = 'grid-square';
+        container.appendChild(gridSquare);
+        gridSquare.addEventListener('mouseover', function(e) {
+            e.target.className = 'hovered';
+        })
+    }
+}
+
+const button = document.querySelector(".btn");
+button.addEventListener('click', gridReset);
